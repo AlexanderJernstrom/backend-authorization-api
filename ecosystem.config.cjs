ff6582718504e194d5aae29ca1b0b560cba01196
AlexanderJernstrom <alexanderjernstrom@gmail.com>
2022-04-17 16:52:08 +0200

    Updated start script

---
+++ ecosystem.config.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: `backend-authorization-api`,
+      script: "./index.js",
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "localhost",
+      },
+      env_development: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+      env_production: {
+        NODE_ENV: process.env.NODE_ENV,
+      },
+    },
+  ],
+};
